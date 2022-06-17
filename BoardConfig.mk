PixelExperience-Devices/device_realme_RMX1971

RMX1971: Write Fingerprint HAL Adapter
vendor.oppo.hardware.biometrics.fingerprint@2.1 -> android.hardware.biometrics.fingerprint@2.1

Change-Id: Id33db73425bb5e92ef5c45e2f8d40fa462942af3
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
Signed-off-by: BabluS <baaswanthmadhav@gmail.com>

BoardConfig.mk
@@ -91,6 +91,7 @@ TARGET_USES_HWC2 := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 

Blaster4385/device_realme_RMX1851_RUI2

BoardConfig.mk
@@ -45,6 +45,9 @@ BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_FS_CONFIG_GEN := $(LOCAL_PATH)/config.fs
 
+# HIDL
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(LOCAL_PATH)/framework_manifest.xml
+
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x00000000