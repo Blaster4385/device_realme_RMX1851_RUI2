bfebd631f912a56fa4038df3119122092e1decb8
Naufal Altaf <nao@altaf.xyz>
2022-07-01 08:27:15 +0700

    RMX1971: Set-up bluetooth stack
    Change-Id: I451c866d72bbe7ef989b6353b75a3a16e5f69164

--- device.mk
+++ device.mk
@@ -49,6 +49,13 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
     tinymix
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
+
 # Init
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
