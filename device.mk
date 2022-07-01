8ec79d9235bc1aac71654f1d64f887f3c5843787
Naufal Altaf <nao@altaf.xyz>
2022-07-01 11:20:06 +0700

    RMX1971: Set-up OMX stack
    Change-Id: Ieac15a3ed927f98a9d1313272b7e91709437210d

--- device.mk
+++ device.mk
@@ -160,6 +160,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.2.vendor
 
+# OMX
+PRODUCT_PACKAGES += \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxG711Enc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
