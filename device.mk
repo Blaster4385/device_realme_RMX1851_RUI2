9828df99fc33fb2208981d9eb70da90b97afabb9
Naufal Altaf <nao@altaf.xyz>
2022-07-01 20:06:10 +0700

    RMX1971: Import stock init files
    Change-Id: I4ff3d9cf7931b0a0af4379c37edfdc90eca33282

--- device.mk
+++ device.mk
@@ -147,7 +147,35 @@ PRODUCT_PACKAGES += \
 
 # Init
 PRODUCT_PACKAGES += \
-    init.recovery.qcom.rc
+    init.recovery.qcom.rc \
+    init.class_main.sh \
+    init.crda.sh \
+    init.mdm.sh \
+    init.oppo.fingerprints.sh \
+    init.oppo.wifi.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.crashdata.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.chg_policy.sh \
+    init.qti.fm.sh \
+    init.qti.ims.sh \
+    qca6234-service.sh
+
+PRODUCT_PACKAGES += \
+    init.msm.usb.configfs.rc \
+    init.oppo.reserve.rc \
+    init.oppo.sensor.rc \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.target.rc
 
 # Keymaster
 PRODUCT_PACKAGES += \
